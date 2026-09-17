b3793eaa9d64118a59270d61c6f3b05edd79e753
int-29 <144659972+int-29@users.noreply.github.com>
2026-09-17 17:16:13 +0300

    make new post form work

--- Gemfile
+++ Gemfile
@@ -66,3 +66,4 @@ group :test do
 end
 
 gem "devise", "~> 5.0"
+gem "json", "~> 2.21"
--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,14 +1,15 @@
 class PostsController < ApplicationController
-  before_action :require_login, only: [ :create, :new ]
+  before_action :authenticate_user!, only: [ :create, :new ]
 
   def new
-    @post = Post.new
+    @post = Post.new()
   end
 
   def create
     @post = Post.new(allowed_post_params)
     if @post.save
       flash[:succes] = "Post created"
+      redirect_to :root
     else
       flash.now[:alert] = "Something went wrong..."
       render :new, status: :unprocessable_entity
@@ -20,14 +21,6 @@ class PostsController < ApplicationController
   end
 
   private
-
-  def require_login
-    if current_user.logged_in?
-      render :new
-    else
-      redirect_to login_path
-    end
-  end
 
   def allowed_post_params
     params.expect(post: [ :title, :body, :user_id ])
